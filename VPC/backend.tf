318dfe1e71fa80b7839472f782f5908a3d12c349
akynbek <sirus_the_best@yahoo.com>
2021-01-24 22:11:21 +0000

    backend updated

--- VPC/backend.tf
+++ VPC/backend.tf
@@ -1,11 +1,7 @@
 terraform {
-  required_version = "0.11.14"
-
-  backend "s3" {
-    bucket = "terraform-state-akon"
-    key    = "path/to/my/key"
-    region = "us-east-1"
-
-    #dynamodb_table = "prog"
+backend "s3" {
+bucket = "terraform-state-akon"
+key = "test/us-east-1/tools/virginia/test.tfstate"
+region = "us-east-1"
   }
 }
