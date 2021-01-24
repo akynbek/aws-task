a42d841a776b0f2e91751c8e26eccc4baa215aa2
akynbek <sirus_the_best@yahoo.com>
2021-01-24 23:39:21 +0000

    updated

--- ASG/module/wordpress.sh
+++ ASG/module/wordpress.sh
@@ -3,4 +3,4 @@ sudo yum install -y epel-release -y
 sudo yum install httpd -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
-echo "Hello-World" > /var/html/index.html
+sudo echo "Hello-World" > /var/html/index.html
